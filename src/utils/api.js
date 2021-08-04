746cff978259081507202f94c757756378e9050e
Joshua Kim <josh_kimmm@yahoo.com>
2021-08-03 19:57:54 -0700

    adding config files

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
+import { DEV_URL, PROD_URL } from './config';
 
 export default axios.create({
-    baseURL: "http://localhost:5000/",
+    baseURL: process.env.NODE_ENV !== "production" ? DEV_URL : PROD_URL,
     responseType: "json"
 });
---
+++ src/utils/config.js
@@ -0,0 +1,2 @@
+export const DEV_URL = "http://localhost:5000/";
+export const PROD_URL = "http://ec2-54-177-87-154.us-west-1.compute.amazonaws.com/";
