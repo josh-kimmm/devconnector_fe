354b0a102c49d788d3a21aafd45ed50a6e885c36
Joshua Kim <josh_kimmm@yahoo.com>
2021-08-05 15:40:31 -0700

    favicon and DNS configuration updates

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,2 +1,2 @@
 export const DEV_URL = "http://localhost:5000/";
-export const PROD_URL = "http://ec2-35-84-150-135.us-west-2.compute.amazonaws.com/";
+export const PROD_URL = "https://devcon.josh-kimmm.com";
