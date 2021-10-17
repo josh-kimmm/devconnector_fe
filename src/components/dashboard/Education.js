09354a5ddf8b9e83ba09330cea4cb54a2848980c
Joshua Kim <josh_kimmm@yahoo.com>
2021-10-17 16:40:12 -0700

    small change again

--- src/components/dashboard/Education.js
+++ src/components/dashboard/Education.js
@@ -29,7 +29,6 @@ const Education = ({ education, deleteEducation }) => {
                         <th>School</th>
                         <th className="hide-sm">Degree</th>
                         <th className="hide-sm">Years</th>
-                        <th />
                     </tr>
                 </thead>
                 <tbody>{educations}</tbody>
